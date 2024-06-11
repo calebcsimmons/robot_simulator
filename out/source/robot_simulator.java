1766dd52cde53a54ed5e6e24e7c2b591ac3a30b9
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-11 16:42:16 -0700

    added start and stop button for simulation

--- out/source/robot_simulator.java
+++ out/source/robot_simulator.java
@@ -4,6 +4,8 @@ import processing.data.*;
 import processing.event.*;
 import processing.opengl.*;
 
+import controlP5.*;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.File;
@@ -15,85 +17,118 @@ import java.io.IOException;
 
 public class robot_simulator extends PApplet {
 
+
+
 PShape base, shoulder, upArm, loArm, end;
 float rotX, rotY;
-float posX=1, posY=50, posZ=50;
+float posX = 1, posY = 50, posZ = 50;
 float alpha, beta, gamma;
 
 float[] Xsphere = new float[99];
 float[] Ysphere = new float[99];
 float[] Zsphere = new float[99];
 
-public void setup(){
+ControlP5 cp5;
+boolean isPlaying = true;
+
+public void setup() {
     /* size commented out by preprocessor */;
-    
+
     base = loadShape("./models/r5.obj");
     shoulder = loadShape("./models/r1.obj");
     upArm = loadShape("./models/r2.obj");
     loArm = loadShape("./models/r3.obj");
     end = loadShape("./models/r4.obj");
-    
+
     shoulder.disableStyle();
     upArm.disableStyle();
-    loArm.disableStyle(); 
+    loArm.disableStyle();
+
+    cp5 = new ControlP5(this);
+    cp5.addButton("Play")
+       .setPosition(10, 10)
+       .setSize(50, 20)
+       .onClick(new CallbackListener() {
+           public void controlEvent(CallbackEvent theEvent) {
+               isPlaying = true;
+           }
+       });
+    cp5.addButton("Stop")
+       .setPosition(70, 10)
+       .setSize(50, 20)
+       .onClick(new CallbackListener() {
+           public void controlEvent(CallbackEvent theEvent) {
+               isPlaying = false;
+           }
+       });
 }
 
-public void draw(){ 
-   writePos();
-   background(32);
-   smooth();
-   lights(); 
-   directionalLight(51, 102, 126, -1, 0, 0);
-    
-    for (int i=0; i< Xsphere.length - 1; i++) {
-    Xsphere[i] = Xsphere[i + 1];
-    Ysphere[i] = Ysphere[i + 1];
-    Zsphere[i] = Zsphere[i + 1];
+public void draw() {
+    background(32);
+    smooth();
+    lights();
+    directionalLight(51, 102, 126, -1, 0, 0);
+
+    if (isPlaying) {
+        writePos();
     }
-    
+
+    for (int i = 0; i < Xsphere.length - 1; i++) {
+        Xsphere[i] = Xsphere[i + 1];
+        Ysphere[i] = Ysphere[i + 1];
+        Zsphere[i] = Ysphere[i + 1];
+    }
+
     Xsphere[Xsphere.length - 1] = posX;
     Ysphere[Ysphere.length - 1] = posY;
-    Zsphere[Zsphere.length - 1] = posZ;
-   
-   noStroke();
-   
-   translate(width/2,height/2);
-   rotateX(rotX);
-   rotateY(-rotY);
-   scale(-4);
-   
-   for (int i=0; i < Xsphere.length; i++) {
-     pushMatrix();
-     translate(-Ysphere[i], -Zsphere[i]-11, -Xsphere[i]);
-     fill (0xFFD003FF, 25);
-     sphere (PApplet.parseFloat(i) / 20);
-     popMatrix();
+    Zsphere[Xsphere.length - 1] = posZ;
+
+    noStroke();
+
+    pushMatrix();  // Save the current matrix state
+
+    translate(width / 2, height / 2);
+    rotateX(rotX);
+    rotateY(-rotY);
+    scale(-4);
+
+    for (int i = 0; i < Xsphere.length; i++) {
+        pushMatrix();
+        translate(-Ysphere[i], -Zsphere[i] - 11, -Xsphere[i]);
+        fill(0xFFD003FF, 25);
+        sphere(PApplet.parseFloat(i) / 20);
+        popMatrix();
     }
-    
-   fill(0xFFFFE308);  
-   translate(0,-40,0);   
-     shape(base);
-     
-   translate(0, 4, 0);
-   rotateY(gamma);
-     shape(shoulder);
-      
-   translate(0, 25, 0);
-   rotateY(PI);
-   rotateX(alpha);
-     shape(upArm);
-      
-   translate(0, 0, 50);
-   rotateY(PI);
-   rotateX(beta);
-     shape(loArm);
-      
-   translate(0, 0, -50);
-   rotateY(PI);
-     shape(end);
+
+    fill(0xFFFFE308);
+    translate(0, -40, 0);
+    shape(base);
+
+    translate(0, 4, 0);
+    rotateY(gamma);
+    shape(shoulder);
+
+    translate(0, 25, 0);
+    rotateY(PI);
+    rotateX(alpha);
+    shape(upArm);
+
+    translate(0, 0, 50);
+    rotateY(PI);
+    rotateX(beta);
+    shape(loArm);
+
+    translate(0, 0, -50);
+    rotateY(PI);
+    shape(end);
+
+    popMatrix();  // Restore the matrix state to prevent GUI transformations
+
+    // Draw ControlP5 elements outside the transformed space
+    cp5.draw();
 }
 
-public void mouseDragged(){
+public void mouseDragged() {
     rotY -= (mouseX - pmouseX) * 0.01f;
     rotX -= (mouseY - pmouseY) * 0.01f;
 }
